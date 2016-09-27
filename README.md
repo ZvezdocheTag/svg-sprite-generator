c63a614541e8171f6fa10b8cd6dcf1f6ebfe68d9
Yastremsky Vitaly <yastremsky@gambit.ru>
2016-09-27 22:30:44 +0300

    gello

--- README.md
+++ README.md
@@ -1,3 +1,57 @@
+## Работа с svg спрайтами
+
+### Сборка
+
+Иконки svg складываем в папку `/images/svg/icons`
+После генерирования в папке `/images/svg`, будут 4 файла:
+
+1. sprite.svg 
+2. metadata.html - визуализация svg спрайта
+3. metadata.json - данные спрайта id иконок и viewBox
+4. data.css - стилизация metadata.html 
+
+Для первого запуска генератора спрайтов, требуется добавить npm пакеты 
+
+Выполняем:
+
+	npm i
+
+Команда для сборки спрайтов:
+
+	gulp svgsprite
+
+Расположения путей для сборки будут уточняться скорее всего это config.json,
+пока файловая система инлайновая напрямую в сборщике, во время интеграции в 
+основной проект будет переписана.
+
+Название иконки генерируется от названия родного файла иконки и к нему
+ добавляется префикс `svg-icon-*`, пример:
+
+**file:**   `star.svg`
+
+**id иконки:**   ` #svg-icon-star`
+
+### Использование
+
+Инлайновая вставка в html:
+
+```html
+    <svg class="svg-icon">
+        <use xlink:href="/path/to/sprite.svg#svg-icon-ok-finger"></use>
+    <svg>
+```
+Стилизация происходит через класс заданный в теге `<svg>`.
+В теге `<use>` указываем путь к спрайту и id нужной нам иконки.
+
+
+#### ISSUE
+
+Доработать использование через css background
+
+
+
+
+
 ## Build svg sprite
 
     gulp svg
@@ -6,12 +60,16 @@
 
     gulp html
 
+
+
 ### Using resources
 
  https://www.npmjs.com/package/gulp-util <br>
  https://github.com/cheeriojs/cheerio  <br>
  https://github.com/gulpjs/vinyl <br>
- http://stackoverflow.com/questions/21617468/node-js-generate-html
+
+ * Генерировать html и контент
+   http://stackoverflow.com/questions/21617468/node-js-generate-html
 
 ### TODO
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
-var svgstore = require('gulp-svgstore');
 var path = require('path');
 var del = require('del');
+var rename = require('gulp-rename');
+var fs = require('fs');
+var runSequence = require('run-sequence');
+
+
+var svgstore = require('gulp-svgstore');
 var svgmin = require('gulp-svgmin')
 var cheerio = require('gulp-cheerio')
 var replace = require('gulp-replace');
-var rename = require('gulp-rename');
 var through2 = require('through2');
 var cheerioTrue = require('cheerio');
 var gutil = require('gulp-util');
-var fs = require('fs');
-var metadataFile = require('./test/metadata.json');
-
+var prettify = require('gulp-html-prettify');
+var beautify = require('gulp-beautify');
 
 gulp.task('svg', function () {
     return gulp
@@ -28,16 +31,14 @@ gulp.task('svg', function () {
                 }]
             }
         }))
-        // remove all fill, style and stroke declarations in out shapes
         .pipe(cheerio({
 			run: function ($) {
-				$('[fill]').removeAttr('fill');
-				$('[stroke]').removeAttr('stroke');
-				$('[style]').removeAttr('style');
+    //             $('[fill]').removeAttr('fill');
+    //             $('[style]').removeAttr('style');
+				// $('defs').find('*').remove();
 			},
 			parserOptions: {xmlMode: true}
 		}))
-		// cheerio plugin create unnecessary string '&gt;', so replace it.
 		.pipe(replace('&gt;', '>'))
         .pipe(svgstore())
         .pipe(through2.obj(function (file, encoding, cb) {
@@ -48,32 +49,57 @@ gulp.task('svg', function () {
             viewBox: $(this).attr('viewBox')
         };
     }).get();
-
     var jsonFile = new gutil.File({
         path: 'metadata.json',
         contents: new Buffer(JSON.stringify(data))
     });
+    var header = '<link rel="stylesheet" href="../main.css">';
+    var body = '' ;
+      
+
+      for(var i = 0; i < data.length; i++) {
+        body +=  
+                '<li class="svg-lib__item">'
+                + '<div class="title">' + data[i].name  + '</div>'
+                + '<div class="img-wrap">'
+                + '<svg class="svg-icon">'
+                + '<use xlink:href="#' + data[i].name  + '"></use>'
+                + '<svg>'
+                +'</div>'
+                +'</li>';
+      }
+    var contentS = $('svg');    
+    var htmlFile = new gutil.File({
+        path: 'metadata.html',
+        contents: new Buffer('<!DOCTYPE html>'
+           + '<html><head>'
+           + header
+           + '</head><body>' + contentS
+           + '<ul class="svg-lib__list">'
+           + body +  '</body></html>')
+    });
+
     this.push(jsonFile);
+    this.push(htmlFile);
     this.push(file);
     cb();
+
 }))
   .pipe(gulp.dest('test'));
 });
-
-gulp.task('clean', function(){ //создаем таск для очишения папки build перед сборкой
-	return del('test');
+gulp.task('templates', function() {
+  gulp.src(['test/metadata.html', 'test/metadata.json'])
+    .pipe(prettify({indent_char: ' ', indent_size: 4}))
+    .pipe(gulp.dest('test/'))
 });
-
-gulp.task('html', function(){ //создаем таск для очишения папки build перед сборкой
-
-  function decorator(f, after, before) {
-    return function(){
-      return before + f.apply(this.arguments) + after
-    }
-  }
-    var dataToAppend ='<!DOCTYPE html>'
-           + '<html><header>'  + '</header><body>'  + '<h1>Htlll</h2>'  +  '</body></html>';
-
-    var $ = cheerioTrue.load(file.contents.toString(), {xmlMode: true});
-    fs.appendFile('data.html', dataToAppend);
+gulp.task('beautify', function() {
+  gulp.src('test/metadata.json')
+    .pipe(beautify({indent_size: 2}))
+    .pipe(gulp.dest('test/'))
+});
+gulp.task('clean', function(){ //создаем таск для очишения папки build перед сборкой
+    return del('test');
+});
+gulp.task('svgsprite', function(callback) {
+  runSequence('clean','svg', 'beautify','templates', callback);
 });
